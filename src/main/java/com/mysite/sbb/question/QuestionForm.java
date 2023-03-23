0a227209f1039d6da0577b3b93f1a840697605dd
jhs512 <jangka512@gmail.com>
2023-03-23 15:33:47 +0900

    폼 필드 속성으로 프론트단에서 유효성 체크

--- src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -1,7 +1,6 @@
 package com.mysite.sbb.question;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
