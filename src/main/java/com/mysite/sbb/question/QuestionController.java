8548400aa8b95064adb003f5fc99cd8e95cb6595
jhs512 <jangka512@gmail.com>
2023-03-23 13:35:13 +0900

    QuestionForm 바인딩 에러 발생시 폼 html 으로 응답

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,7 +46,13 @@ public class QuestionController {
     @PostMapping("/create")
     // @Valid QuestionForm questionForm
     // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
-    public String questionCreate(@Valid QuestionForm questionForm) {
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if ( bindingResult.hasErrors() ) {
+            // question_form.html 실행
+            // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
+            return "question_form";
+        }
+
         questionService.create(questionForm.getSubject(), questionForm.getContent());
 
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
